d243e5a402e1ee7a7ede20fb51e76582f30eb735
Nata-Omega7774 <Nata-Omega7774@yandex.ru>
2025-06-28 08:39:18 +0400

    lesson_03

--- lesson_03/lesson_3_task_3.py
+++ lesson_03/lesson_3_task_3.py
@@ -3,7 +3,7 @@ from mailing import Mailing
 
 to_address = Address("426035", "Ижевск", "Льва Толстого", "17", "33")
 from_address = Address("427000", "Сарапул", "Ленина", "170", "23")
-mailing = Mailing(to_address, from_address, 426, "AFJ345")
+mailing = Mailing(to_address, from_address, 105, "AFJ345")
 
 print(f"Отправление {mailing.track} из {mailing.from_address.index}, {mailing.from_address.city},"
       f" {mailing.from_address.street}, {mailing.from_address.house} - {mailing.from_address.apartament} "
