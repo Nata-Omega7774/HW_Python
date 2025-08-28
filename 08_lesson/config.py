f3670e311c045153bccd8333d8d62d7f42f8e501
Nata-Omega7774 <Nata-Omega7774@yandex.ru>
2025-08-28 22:09:05 +0400

    Удаление ненужных файлов 08_lesson

--- 08_lesson/config.py
+++
@@ -1,3 +0,0 @@
-# config.py
-# Наставнику: замените этот токен на реальный API токен Yougile
-API_TOKEN = "VOAM+5iCIMbPImLN8av7VcHi57iUgb0cnGfeHoYc0i+Q8un7D3zJg82WyaHsSjs4"
--- 08_lesson/constants.py
+++
@@ -1,4 +0,0 @@
-# Первое задание
-X_client_URL = "https://x-clients-be.onrender.com"
-# Второе задание
-Todo_list_URL = "https://sky-todo-list.herokuapp.com"
